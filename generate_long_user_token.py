a9832ef02af88b27b2f8ad83d0a6630528519718
Mo <mooaz09@gmail.com>
2021-09-23 16:44:15 -0700

    Instagram Code Helper

--- generate_long_user_token.py
+++ generate_long_user_token.py
@@ -5,7 +5,7 @@ url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_toke
 tokens = open('generate_user_token_values.txt')
 client_id = tokens.readline().split(":")[1]
 client_secret = tokens.readline().split(":")[1]
-fb_exchange_token = tokens.readline().split(":")[1]
+fb_exchange_token = tokens.readline().split(":")[1]  # place short lived access token to get long lived access token
 file = tokens.readline().split(":")[1]
 path = tokens.readline().split(":")[1]
 tokens.close()
@@ -17,7 +17,7 @@ querystring = {"client_id": client_id,
 headers = {"Accept": "application/json"}
 response = requests.request("GET", url, headers=headers, params=querystring)
 long_life_access_token = response.json()['access_token']
-os.remove(path)
+os.remove(path)  
 token_file = open(file, 'w')
 token_file.write(long_life_access_token)
 token_file.close()
