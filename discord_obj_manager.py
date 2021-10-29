7f13ed14122bb8c701a81d47c1a0478c2a7e8517
Mo <mooaz09@gmail.com>
2021-10-29 14:08:19 -0700

    Discord Code.

---
+++ discord_obj_manager.py
@@ -0,0 +1,6 @@
+from post_to_discord_manager import ManageManager
+
+mm = ManageManager('Sales DB .json', 'Listings DB .json', 'Discord Keys .txt', 'Discord Values .txt')
+mm.run()
+
+# explain files
