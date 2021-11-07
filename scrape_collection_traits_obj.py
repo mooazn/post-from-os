6c31698e52b4ac3d8c5de6f821a01bcf97eb7292
Mo <mooaz09@gmail.com>
2021-11-06 17:32:57 -0700

    Scrape Collection Traits.

--- scrape_collection_traits_obj.py
+++ scrape_collection_traits_obj.py
@@ -1,6 +1,6 @@
 from scrape_collection_traits import ScrapeCollectionTraits
 
-sct = ScrapeCollectionTraits('DB Name .json', 'Collection Name', 'Contract Address', 123)
+sct = ScrapeCollectionTraits('DB Name.json', 'Collection Name', 123)
 
 # This file scrapes traits for the provided collection. The parameters are explained below:
 
@@ -11,10 +11,8 @@ sct = ScrapeCollectionTraits('DB Name .json', 'Collection Name', 'Contract Addre
 # on the main page of the collection on OpenSea. The easiest way is to extract the name from the URL. for example:
 # https://opensea.io/collection/boredapeyachtclub. You would extract 'boredapeyachtclub' and put that as the 2nd param.
 
-# The third parameter is simply the contract address of the collection. I will remove this parameter later since I can
-# extract that myself with the correct name.
-
-# The last parameter is not necessary. However, it is preferred to provide it if you know the MAX COLLECTION SIZE (not
+# The last parameter is not necessary. It is defaulted to 4000.
+# However, it is preferred to provide it if you know the MAX COLLECTION SIZE (not
 # the number there are right now). A collection could be still be minting and have 2000/5000 total so far. You MUST
 # provide 5000 in this case, not 2000. another case is mints in the same project. for instance, if a project has
 # "SOME NFT #2059" and they release a new project which maps the previous ID to the new one, it would look like
@@ -22,5 +20,4 @@ sct = ScrapeCollectionTraits('DB Name .json', 'Collection Name', 'Contract Addre
 # really only makes sense to use for projects that have finished minting completely so you get accurate results.
 
 # Ex.
-# sct = ScrapeCollectionTraits('humanoids_trait_db.json', 'thehumanoids', '0x3a5051566b2241285be871f650c445a88a970edd',
-#                              10000)
+# sct = ScrapeCollectionTraits('humanoids_trait_db.json', 'thehumanoids', 10000)
