eed0b8513fbfeb87d4fcf46c122535576d3aedab
Mo <mooaz09@gmail.com>
2022-05-11 00:58:30 -0700

    create sshActivity

--- AndroidApp/app/src/main/java/com/example/servermanagerapp/sshActivity.java
+++ AndroidApp/app/src/main/java/com/example/servermanagerapp/sshActivity.java
@@ -1,4 +1,15 @@
 package com.example.servermanagerapp;
 
-public class sshActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class sshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ssh);
+    }
 }
