7a2248c95b2151ab32f40ee81b6e6fc3a8f68db4
Mo <mooaz09@gmail.com>
2022-08-21 15:43:16 -0700

    add print statements for now

--- HelperCode/begin_server.py
+++ HelperCode/begin_server.py
@@ -3,16 +3,19 @@ import time
 
 
 def begin_server():
+    os.system(f'tmux kill-server')
+    print('All tmux sessions killed.')
     home = os.getcwd()
-    config_file = open('sample_config.txt', 'r')
+    config_file = open('config.txt', 'r')
     for path in config_file.readlines():
         cur_path = path.strip().split('/')
-        cur_directory = '/'.join(cur_path[0:4])
+        cur_directory = '/'.join(cur_path[0:-1])
         cur_file_name = cur_path[-1]
         os.chdir(cur_directory)
         os.system(f'tmux new -d -s {cur_file_name[0:-3]}')
         os.system(f'tmux send-keys -t {cur_file_name[0:-3]} "python3 {cur_file_name}" enter')
-        time.sleep(5)
+        print(f'Started {cur_file_name} successfully.')
+        time.sleep(2.5)
     os.chdir(home)
     config_file.close()
 
